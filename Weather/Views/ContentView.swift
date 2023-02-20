b81a5134f4e7ed3439e3f0902a59e8465095f65e
Sujay Ghosh <sujayghosh@Sujays-MacBook-Air.local>
2023-02-20 18:28:16 +0530

    Weather app is added

---
+++ Weather/Views/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Sujay Ghosh on 11/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location {
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else{
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather:\(error)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
+        .background(Color(hue: 0.678, saturation: 0.921, brightness: 0.38))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
